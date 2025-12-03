7b85cc75cf2f79adb5f4d4e2daa957799a295595
hesan <hesan@HEZZ>
2025-12-03 14:05:37 +0100

    Add project files.

---
+++ BankAccount/BankAccount.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+	int bank_balance = 1000;
+	int rate_year = 2;
+
+	printf("The account balance after 1st year is, %d Euros\n ", (bank_balance + (bank_balance * 2)));
+	printf("The account balance after 2nd year is, %d Euros\n ", (bank_balance + (bank_balance * 2))+ ((bank_balance + (bank_balance * 2))*2));
+	printf("The account balance after 3rd year is, %d Euros\n ", (bank_balance + (bank_balance * 2)) + ((bank_balance + (bank_balance * 2)) * 2)+ ((bank_balance + (bank_balance * 2)) + ((bank_balance + (bank_balance * 2)) * 2)*2));
+
+	getchar();
+	return 0;
+}
---
+++ Blub/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void) {
+	printf("Hey!");
+
+	getchar();
+	return 0;
+}
+
