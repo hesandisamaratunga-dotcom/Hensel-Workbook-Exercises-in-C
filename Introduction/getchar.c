0cb734b6da7bae36d2dfdaeee0ad76b6c0f40db5
Hezz__s_ <hesandisamaratunga@gmail.com>
2025-12-27 16:16:48 +0100

    Add files via upload

---
+++ Introduction/getchar.c
@@ -0,0 +1,13 @@
+/* Include files */
+#include <stdio.h>
+
+/* Main function */
+int main(void)
+{
+	printf("Wow, this really shows up on the console!\n");
+	getchar();
+	getchar();
+	getchar();
+
+	return 0;
+}
---
+++ Introduction/printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hesandi Samarathunga \n\n");
+	printf("I was born in Sri Lanka \n");
+	getchar();
+	return 0;
+}
+
+
+
+
+
