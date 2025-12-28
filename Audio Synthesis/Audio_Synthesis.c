b7f6c79e05edfb0fa3cd5dea68df51aac25ab4f2
Hezz__s_ <hesandisamaratunga@gmail.com>
2025-12-28 14:39:11 +0100

    Add files via upload

---
+++ Audio Synthesis/Audio_Synthesis.c
@@ -0,0 +1,6 @@
+﻿Build started at 14:24...
+1>------ Build started: Project: AudioSynthesis, Configuration: Debug x64 ------
+1>AudioSynthesis.c
+1>AudioSynthesis.vcxproj -> D:\Desktop\Visual studio projects\AudioSynthesis\x64\Debug\AudioSynthesis.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 14:24 and took 00.397 seconds ==========
